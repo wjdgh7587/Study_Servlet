ddfc72f0345547deac8b5aef69a7eb40fe0840d1
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-30 12:16:56 +0900

    Update Mapping 경로 수정

--- servlet-connection-pool02-1130/src/kr/or/ksmart/controller/MemberController.java
+++ servlet-connection-pool02-1130/src/kr/or/ksmart/controller/MemberController.java
@@ -13,7 +13,7 @@ import kr.or.ksmart.service.MemberService;
  * Servlet implementation class MemberController
  */
 //@WebServlet({ "/MemberController", "*.m" })
-@WebServlet({ "/member/", "*.do" })
+@WebServlet("/member/*")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -33,13 +33,13 @@ public class MemberController extends HttpServlet {
 		// URI 호출후, ContextPath() 부분 공백 처리
 		uri = uri.replace(request.getContextPath(), "");
 		//치환이 어떻게 작동하는지 하나씩 코드를 돌려보는것도 좋을 것 같다.
-		if ("/member/*.do".equals(uri)) {
+		if ("/member/*".equals(uri)) {
 			// memberList로 mapping
-			response.sendRedirect(request.getContextPath() + "/member/memberList.do");
-		} else if ("/member/memberList.do".equals(uri)) {
+			response.sendRedirect(request.getContextPath() + "/member/memberList");
+		} else if ("/member/memberList".equals(uri)) {
 			memberService.getMemberList();
 			// 서비스 호출
-			System.out.println("/memberList.m 호출 완료");
+			System.out.println("/memberList 호출 완료");
 			// 생성자 메소드딴에서 호출하지 않고 호출
 			//MemberService memberService = new MemberService();
 			//생성자 메소드로 호출 결국은 똑같다.
