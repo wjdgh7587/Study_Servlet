0da312bff7a1ef9bbe2499ff0e39a03344a564ac
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-30 12:32:25 +0900

    Update mapping

--- servlet-connection-pool02-1130/WebContent/WEB-INF/view/member/memberList.jsp
+++ servlet-connection-pool02-1130/WebContent/WEB-INF/view/member/memberList.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>회원정보</title>
 </head>
 <body>
-
+	<h1>회원 목록</h1>
 </body>
 </html>
--- servlet-connection-pool02-1130/src/kr/or/ksmart/controller/MemberController.java
+++ servlet-connection-pool02-1130/src/kr/or/ksmart/controller/MemberController.java
@@ -18,6 +18,9 @@ public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private MemberService memberService;
+	private final static String SERVICE_NAME = "/member/";
+	private final static String VIEW_PATH = "/WEB-INF/view/member/";
+	//final은 대문자로 선언한다. 변하지 않는 것들
 	
 	public MemberController() {
 		super();
@@ -33,21 +36,34 @@ public class MemberController extends HttpServlet {
 		// URI 호출후, ContextPath() 부분 공백 처리
 		uri = uri.replace(request.getContextPath(), "");
 		//치환이 어떻게 작동하는지 하나씩 코드를 돌려보는것도 좋을 것 같다.
-		if ("/member/*".equals(uri)) {
+		
+		//Forward처리
+		String forwardPath = null;
+		
+		if (uri.equals(SERVICE_NAME+"*")) {
 			// memberList로 mapping
-			response.sendRedirect(request.getContextPath() + "/member/memberList");
-		} else if ("/member/memberList".equals(uri)) {
+			response.sendRedirect(request.getContextPath() + SERVICE_NAME+"memberList");
+		} else if (uri.equals(SERVICE_NAME+"memberList")) {
 			memberService.getMemberList();
 			// 서비스 호출
-			System.out.println("/memberList 호출 완료");
 			// 생성자 메소드딴에서 호출하지 않고 호출
 			//MemberService memberService = new MemberService();
 			//생성자 메소드로 호출 결국은 똑같다.
 			memberService.getMemberList();
+			
+			forwardPath = "memberList";
 
+			System.out.println(SERVICE_NAME+"/memberList 호출 완료");
 		} else {
 			// 아닐경우 404 에러 응답이 제대로 되면 202,
 			response.setStatus(404);
+		}
+		/********************************************************
+		 * forwardpath가 null이 아니라면 forward 실행
+		 ********************************************************/
+
+		if(forwardPath != null) {
+			request.getRequestDispatcher(VIEW_PATH+ forwardPath +".jsp").forward(request, response);
 		}
 	}
 
